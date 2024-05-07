afb79e02a1e73ac3d89121b975815cd68c4d0f46
jerripat <jerripatterson@gmail.com>
2024-05-06 19:37:04 -0500

    alot of changes and additions. Everything works at this commit

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 
-
 # Create your views here.
 def starting_page(request):
     return render(request, "blog/index.html")
 
-def posts(request):
-    return render(request, "posts.html")
 
-def post_details(request):
-    return render(request, "post_details.html")
+def posts(request):
+    return render(request, "blog/all-posts.html")
+
+
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")
