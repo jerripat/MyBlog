07dcbf8fd74ed95081228b4e193b1ef4c5a31e8e
jerripat <jerripatterson@gmail.com>
2024-05-08 10:18:25 -0500

    alot of changes and additions. Everything works at this commit added post,author,tag model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("tags", "author", "date")
+    list_display = ("title", "author", "date")
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+class Tag(models.Model):
+    caption = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.caption
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    excerpt = models.CharField(max_length=200)
+    image_name= models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
 
 # Create your models here.
