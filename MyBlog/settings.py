90e6305967e802c4f8076292fd3576476471dda0
jerripat <jerripatterson@gmail.com>
2025-12-27 12:46:03 -0600

    Reload

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -1,8 +1,5 @@
 
 import os
-
-
-
 from pathlib import Path
 
 
@@ -110,8 +107,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]  # keep this since diffsettings shows you use it
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
